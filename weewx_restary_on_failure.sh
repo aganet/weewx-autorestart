e5f130c96a52cef2ca6d4729ec7b6b72aa2fcd28
Georgios Anestopoulos <giorgos327@gmail.com>
2024-02-20 07:41:25 +0000

    Create weewx_restary_on_failure.sh

---
+++ weewx_restary_on_failure.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+echo "check that the job is running" > /var/log/weewx_cron.log
+
+# Define the name of the service
+service_name="weewx"
+
+current_time=$(date +%Y-%m-%d\ %T)
+message="WEEWX was found stopped and restarted at: $current_time"
+
+# Check the status of the service
+status=$(systemctl show $service_name | grep "SubState")
+
+# If the status is "exited" or "failed", restart the service
+if [ "$status" == "SubState=exited" ] || [ "$status" == "SubState=failed" ]; then
+  systemctl stop $service_name
+  sleep 10
+  systemctl start $service_name
+  echo "$message" >> /var/log/weewx_restart.log
+fi
